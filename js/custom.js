f6b160c447b9519b82437c8ca3825dfcef61ecb1
JustSergiio <sergivb01@gmail.com>
2016-04-17 17:54:56 +0200

    Added many files

---
+++ js/custom.js
@@ -0,0 +1,49 @@
+//Notification closer.
+$(document).ready(function(){
+
+    $( ".update-close" ).click(function() {
+        $(this).parent().parent().hide();
+    });
+
+
+//Scroll to top
+
+    $(document).on( 'scroll', function(){
+ 
+        if ($(window).scrollTop() > 100) {
+            $('.scroll-top-wrapper').addClass('show');
+        } else {
+            $('.scroll-top-wrapper').removeClass('show');
+        }
+        $('.material-button-toggle').click(function () {
+        $(this).toggleClass('open');
+        $('.option').toggleClass('scale-on');
+    });
+    });
+ 
+    $('.scroll-top-wrapper').on('click', scrollToTop);
+ 
+function scrollToTop() {
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 2500, 'linear');
+}
+
+//Scrolling nav
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+$('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
